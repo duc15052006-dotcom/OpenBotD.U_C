efc551e229f25105b8e0386a9b7fa4c9702e8be1
duc15052006-dotcom <duc15052006@gmail.com>
2026-09-18 14:22:13 +0700

    test(desktop): cover model probe metadata SSRF floor

--- desktop/src-tauri/src/model_connection_tests.rs
+++ desktop/src-tauri/src/model_connection_tests.rs
@@ -14,6 +14,38 @@ fn compatible_probe_keeps_local_http_available() {
     assert_eq!(url.as_str(), "http://127.0.0.1:11434/v1/models");
 }
 
+
+#[test]
+fn compatible_probe_refuses_cloud_metadata_endpoints_even_when_local_http_is_supported() {
+    for endpoint in [
+        "http://169.254.169.254/latest/meta-data",
+        "http://169.254.170.2/v2/credentials",
+        "http://100.100.100.200/latest/meta-data",
+        "http://metadata.google.internal/computeMetadata/v1",
+        "http://metadata.goog/computeMetadata/v1",
+        "http://[fd00:ec2::254]/latest/meta-data",
+    ] {
+        assert!(
+            models_probe_url(endpoint).is_err(),
+            "{endpoint} unexpectedly became a model probe target"
+        );
+    }
+}
+
+#[test]
+fn compatible_probe_catches_ipv4_metadata_hidden_in_ipv6_forms() {
+    for host in [
+        "::ffff:169.254.169.254",
+        "64:ff9b::a9fe:a9fe",
+        "::a9fe:a9fe",
+    ] {
+        assert!(
+            model_probe_never_allowed_host(host),
+            "{host} unexpectedly bypassed the metadata-address floor"
+        );
+    }
+}
+
 #[test]
 fn compatible_probe_refuses_non_http_schemes_and_missing_hosts() {
     for endpoint in [
