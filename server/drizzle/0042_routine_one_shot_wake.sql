f7294b4e756f57ebf743b61e801a1b25c498bd94
duc15052006-dotcom <duc15052006@gmail.com>
2026-09-19 19:03:55 +0700

    fix(migrations): keep one-shot snapshot type spelling aligned

--- server/drizzle/0042_routine_one_shot_wake.sql
+++ server/drizzle/0042_routine_one_shot_wake.sql
@@ -1,2 +1,2 @@
 CREATE TYPE "public"."routine_schedule_kind" AS ENUM('recurring', 'once');--> statement-breakpoint
-ALTER TABLE "routines" ADD COLUMN "schedule_kind" "routine_schedule_kind" DEFAULT 'recurring' NOT NULL;
+ALTER TABLE "routines" ADD COLUMN "schedule_kind" routine_schedule_kind DEFAULT 'recurring' NOT NULL;
